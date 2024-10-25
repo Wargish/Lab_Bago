5e93731fa086ecec65528a139c7de2a7bd13d621
Patricio Morales <78184534+Wargish@users.noreply.github.com>
2024-10-25 10:57:50 -0300

    avances 25/10
    Se agrega modelo de tarea y reporte

--- app/forms.py
+++ app/forms.py
@@ -74,14 +74,22 @@ class InformeForm(forms.ModelForm):
         }
 
 
+class TareaForm(forms.ModelForm):
+    class Meta:
+        model = Tarea
+        fields = ['informe', 'objetivo', 'estado', 'tecnico']
+        widgets = {
+            'informe': forms.Select(attrs={'class': 'form-control'}),
+            'objetivo': forms.TextInput(attrs={'class': 'form-control'}),
+            'estado': forms.Select(attrs={'class': 'form-control'}),
+            'tecnico': forms.Select(attrs={'class': 'form-control'}),
+        }
+
 class ReporteForm(forms.ModelForm):
     class Meta:
         model = Reporte
-        fields = ['user', 'lugar', 'objetivo', 'mensaje', 'image',]
+        fields = ['contenido', 'imagen']
         widgets = {
-            'user': forms.Select(attrs={'class': 'form-control'}),
-            'lugar': forms.Select(attrs={'class': 'form-control'}),
-            'objetivo': forms.TextInput(attrs={'class': 'form-control'}),
-            'mensaje': forms.Textarea(attrs={'class': 'form-control'}),
-            'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
+            'contenido': forms.Textarea(attrs={'class': 'form-control'}),
+            'imagen': forms.ClearableFileInput(attrs={'class': 'form-control'}),
         }
--- app/models.py
+++ app/models.py
@@ -32,17 +32,16 @@ class InformeCondiciones(models.Model):
     lugar = models.ForeignKey(Lugar, on_delete=models.SET_NULL, null=True)
     objetivo = models.TextField(null=True, blank=True)
     mensaje = models.TextField(null=True, blank=True)
-    estado = models.ForeignKey(Estado, on_delete=models.SET_NULL, null=True, default=None)
-    image = models.ImageField(upload_to='app/static/images/')
+    image = models.ImageField(upload_to='app/static/images/', blank=False)
     tipo_Informe = models.CharField(max_length=3, choices=TIPO_REPORTE_CHOICES, default=INFRASTRUCTURA)
 
+    def meta():
+        ordering = ['-createdAt']
+
     def save(self, *args, **kwargs):
-        if not self.estado:
-            estado_pendiente = Estado.objects.filter(nombre='Pendiente').first()
-            if estado_pendiente:
-                self.estado = estado_pendiente
         super().save(*args, **kwargs)
         self.generar_notificaciones()
+
 
     def generar_notificaciones(self):
         tecnicos = Group.objects.get(name='Técnicos').user_set.all()
@@ -70,12 +69,46 @@ class Notificacion(models.Model):
         return f'Notificación para {self.user.username}: {self.mensaje.objetivo}'
     
 
-class Reporte(models.Model):
-    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    lugar = models.ForeignKey(Lugar, on_delete=models.SET_NULL, null=True)
+class Tarea(models.Model):
+    informe = models.ForeignKey(InformeCondiciones, on_delete=models.CASCADE)
     objetivo = models.TextField(null=True, blank=True)
-    mensaje = models.TextField(null=True, blank=True)
-    image = models.ImageField(upload_to='app/static/images/')
+    estado = models.CharField(max_length=20,
+                              choices= [
+                                    ('Pendiente', 'Pendiente'),
+                                    ('En Curso', 'En Curso'),
+                                    ('Completada', 'Completada'),
+                                    ('Archivada', 'Archivada'),
+                                    ],
+                                    default='Pendiente'
+                            )
+    tecnico = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def aceptar(self, tecnico):
+        self.tecnico = tecnico
+        self.estado = 'En Curso'
+        self.save()
+
+    def marcar_como_completada(self):
+        self.estado = 'Completada'
+        self.save()
+
+    def archivada(self):
+        self.estado = 'Archivada'
+        self.save()
 
     def __str__(self):
-        return f'Reporte: {self.objetivo}'
+        return f'Tarea: {self.objetivo} - Estado: {self.estado}'
+    
+
+class Reporte(models.Model):
+    tarea = models.ForeignKey(Tarea, on_delete=models.CASCADE)
+    contenido = models.TextField(null=True, blank=True)
+    imagen = models.ImageField(upload_to='app/static/images/', blank=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Reporte de Tarea: {self.tarea.objetivo} - Fecha: {self.createdAt.strftime("%Y-%m %H:%M")}'
+
+    
+
